gfto/Server

commit a95f6d84f0674d98bc72fbd5d81bdcad412e8d0d

Improved read_fps.

modules/ffmpeg/producer/util/util.cpp
@@ -336,7 +336,15 @@ double read_fps(AVFormatContext& context, double fail_value)
 	{
 		const auto video_context = context.streams[video_index]->codec;
 		const auto video_stream  = context.streams[video_index];
-						
+				
+		auto frame_rate_time_base = video_stream->avg_frame_rate;
+		std::swap(frame_rate_time_base.num, frame_rate_time_base.den);
+
+		if(is_sane_fps(frame_rate_time_base))
+		{
+			return static_cast<double>(frame_rate_time_base.den) / static_cast<double>(frame_rate_time_base.num);
+		}
+
 		AVRational time_base = video_context->time_base;
 
 		if(boost::filesystem2::path(context.filename).extension() == ".flv")
@@ -463,6 +471,30 @@ bool is_valid_file(const std::wstring filename)
 	static const std::vector<std::wstring> invalid_exts = boost::assign::list_of(L".png")(L".tga")(L".bmp")(L".jpg")(L".jpeg")(L".gif")(L".tiff")(L".tif")(L".jp2")(L".jpx")(L".j2k")(L".j2c")(L".swf")(L".ct");
 	
 	return is_valid_file(filename, invalid_exts);
+}
+
+bool try_get_duration(const std::wstring filename, std::int64_t& duration, boost::rational<std::int64_t>& time_base)
+{		
+	AVFormatContext* weak_context = nullptr;
+	if(avformat_open_input(&weak_context, narrow(filename).c_str(), nullptr, nullptr) < 0)
+		return false;
+
+	std::shared_ptr<AVFormatContext> context(weak_context, av_close_input_file);
+	
+	context->probesize = context->probesize / 10;
+	context->max_analyze_duration = context->probesize / 10;
+
+	if(avformat_find_stream_info(context.get(), nullptr) < 0)
+		return false;
+
+	const auto fps = read_fps(*context, 1.0);
+		
+	const auto rational_fps = boost::rational<std::int64_t>(static_cast<int>(fps * AV_TIME_BASE), AV_TIME_BASE);
+	
+	duration = boost::rational_cast<std::int64_t>(context->duration * rational_fps / AV_TIME_BASE);
+	time_base = 1/rational_fps;
+
+	return true;
 }
 
 std::wstring probe_stem(const std::wstring stem, const std::vector<std::wstring>& invalid_exts)

casparcg.sln
@@ -1,6 +1,6 @@
 ﻿
 Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio 2010
+# Visual Studio 2012
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "core", "core\core.vcxproj", "{79388C20-6499-4BF6-B8B9-D8C33D7D4DDD}"
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "shell", "shell\shell.vcxproj", "{8C26C94F-8092-4769-8D84-DEA479721C5B}"

modules/ffmpeg/producer/util/util.h
@@ -27,6 +27,13 @@
 #include <core/producer/frame/pixel_format.h>
 #include <core/mixer/audio/audio_mixer.h>
 
+#include <boost/rational.hpp>
+#include <boost/optional.hpp>
+
+#include <tuple>
+
+#include <cstdint>
+
 enum PixelFormat;
 struct AVFrame;
 struct AVFormatContext;
@@ -75,6 +82,7 @@ std::wstring probe_stem(const std::wstring stem, const std::vector<std::wstring>
 std::wstring probe_stem(const std::wstring stem);
 bool is_valid_file(const std::wstring filename, const std::vector<std::wstring>& invalid_exts);
 bool is_valid_file(const std::wstring filename);
+bool try_get_duration(const std::wstring filename, std::int64_t& duration, boost::rational<std::int64_t>& time_base);
 
 core::channel_layout get_audio_channel_layout(const AVCodecContext& context, const std::wstring& custom_channel_order);
 

protocol/amcp/AMCPCommandsImpl.cpp
@@ -201,20 +201,30 @@ std::wstring MediaInfo(const boost::filesystem::wpath& path)
 {
 	if(boost::filesystem::is_regular_file(path))
 	{
+		std::int64_t duration = 0;
+		boost::rational<std::int64_t> time_base;
+				
 		std::wstring clipttype = TEXT(" N/A ");
 		std::wstring extension = boost::to_upper_copy(path.extension());
 		if(extension == TEXT(".TGA") || extension == TEXT(".COL") || extension == L".PNG" || extension == L".JPEG" || extension == L".JPG" ||
 			extension == L"GIF" || extension == L"BMP")
-			clipttype = TEXT(" STILL ");
+		{
+			clipttype = TEXT(" STILL ");			
+		}
 		else if(extension == TEXT(".WAV") || extension == TEXT(".MP3"))
+		{
 			clipttype = TEXT(" AUDIO ");
+		}
 		else if(extension == TEXT(".SWF") || extension == TEXT(".CT") || 
 			    extension == TEXT(".DV") || extension == TEXT(".MOV") || 
 				extension == TEXT(".MPG") || extension == TEXT(".AVI") || 
 				extension == TEXT(".MP4") || extension == TEXT(".FLV") || 
 				extension == TEXT(".STGA") || 
 				caspar::ffmpeg::is_valid_file(path.file_string()))
+		{
+			ffmpeg::try_get_duration(path.file_string(), duration, time_base);
 			clipttype = TEXT(" MOVIE ");
+		}
 
 		if(clipttype != TEXT(" N/A "))
 		{		
@@ -233,19 +243,22 @@ std::wstring MediaInfo(const boost::filesystem::wpath& path)
 			auto str = relativePath.replace_extension(TEXT("")).external_file_string();
 			if(str[0] == '\\' || str[0] == '/')
 				str = std::wstring(str.begin() + 1, str.end());
-
-			return std::wstring() + TEXT("\"") + str +
-					+ TEXT("\" ") + clipttype +
-					+ TEXT(" ") + sizeStr +
-					+ TEXT(" ") + writeTimeWStr +
-					+ TEXT("\r\n"); 	
+			
+			return std::wstring() 
+					+ L"\""		+ str +
+					+ L"\" "	+ clipttype +
+					+ L" "		+ sizeStr +
+					+ L" "		+ writeTimeWStr +
+					+ L" "		+ boost::lexical_cast<std::wstring>(duration) +
+					+ L" "		+ boost::lexical_cast<std::wstring>(time_base.numerator()) + L"/" + boost::lexical_cast<std::wstring>(time_base.denominator())
+					+ L"\r\n"; 	
 		}	
 	}
 	return L"";
 }
 
 std::wstring ListMedia()
-{	
+{		
 	std::wstringstream replyString;
 	for (boost::filesystem::wrecursive_directory_iterator itr(env::media_folder()), end; itr != end; ++itr)	
 		replyString << MediaInfo(itr->path());

commit 51b588f3b5a53122988ce7391e37f4638590650d

Fix regression in playing progressive clips.
This fixes regression introduced in a95f6d84f. The problem is
that using avg_frame_rate causes fps to double for progressive
clips which is not what we want.

This patch removes the only place where avg_frame_rate was used
and fixes issue #264.

modules/ffmpeg/producer/util/util.cpp
@@ -338,14 +338,6 @@ double read_fps(AVFormatContext& context, double fail_value)
 	{
 		const auto video_context = context.streams[video_index]->codec;
 		const auto video_stream  = context.streams[video_index];
-					
-		auto frame_rate_time_base = video_stream->avg_frame_rate;
-		std::swap(frame_rate_time_base.num, frame_rate_time_base.den);
- 
-		if(is_sane_fps(frame_rate_time_base))
-		{
-			return static_cast<double>(frame_rate_time_base.den) / static_cast<double>(frame_rate_time_base.num);
-		}
 
 		AVRational time_base = video_context->time_base;
 